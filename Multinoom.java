538c4c709ce7ec1f2dfe7a8f460e5c008787da0b
intermetric <randoleppik@gmail.com>
2019-04-02 17:11:35 +0300

    Muudetud peaklass, lisatud pseudoklassid

---
+++ Multinoom.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Multinoom implements Arvutatav {
+
+    private int k;
+    private int n;
+    private double p;
+
+    public void setK(int k) {
+        this.k = k;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public void setP(double p) {
+        this.p = p;
+    }
+
+    public Multinoom() {
+        this.k = 0;
+        this.n = 0;
+        this.p = 0.0;
+    }
+
+    static long kombinatsioonid(int k, int n) {
+        long lugeja = 1L;
+        long nimetaja = 1L;
+        if (k > n - k) {
+            for (int i = k + 1; i < n + 1; i++) {
+                lugeja *= i;
+            }
+            for (int i = 1; i < n - k + 1; i++) {
+                nimetaja *= i;
+            }
+        } else {
+            for (int i = n - k + 1; i < n + 1; i++) {
+                lugeja *= i;
+            }
+            System.out.println();
+            for (int i = 1; i < k + 1; i++) {
+                nimetaja *= i;
+            }
+        }
+        return lugeja / nimetaja;
+    }
+
+    public void arvuta() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Sisesta p (vaadeldava sündmuse toimumise tõenäosus, mis on igal katsel sama)");
+        setP(scan.nextDouble());
+        System.out.println("Sisesta n (kõikide katsete koguarv)");
+        setN(scan.nextInt());
+        System.out.println("Sisesta k (see arv, mitmel korral sündmus toimub)");
+        setK(scan.nextInt());
+        double tõenäosus = kombinatsioonid(k, n) * Math.pow(p, k) * Math.pow((1 - p), (n - k));
+        System.out.println("Igal katsel tõenäosusega " + p + " toimuva sündmuse puhul tõenäosus, et " + n + "-st katsest" +
+                " sündmus toimub " + k + " katsel, on: " + Math.round(tõenäosus * 1000) / 1000.0);
+    }
+
+}
--- Peaklass.java
+++ Peaklass.java
@@ -5,12 +5,51 @@ public class Peaklass {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Kui soovid kasutada Bernoulli valemit - sisesta '1' \n                   " +
-                " Multinoomvalemit - sisesta '2' \n                    Hüpergeomeetrilist valemit - sisesta '3' " +
-                " \n                    Bernoulli jaotust - sisesta '4' \n                    " +
-                "Binoomjaotust - sisesta '5' \n                    Geomeetrilist jaotust - sisesta '6' \n " +
-                "                   Poissoni jaotust - sisesta '7'.");
-        int sisestus = scan.nextInt();
 
+        int sisestus = 0;
+        while (sisestus != 8) {
+            System.out.println("Kui soovid kasutada Bernoulli valemit - sisesta '1' \n                   " +
+                    " Multinoomvalemit - sisesta '2' \n                    Hüpergeomeetrilist valemit - sisesta '3' " +
+                    " \n                    Bernoulli jaotust - sisesta '4' \n                    " +
+                    "Binoomjaotust - sisesta '5' \n                    Geomeetrilist jaotust - sisesta '6' \n " +
+                    "                   Poissoni jaotust - sisesta '7'.\n                    Väljumiseks sisesta '8'.");
+
+            sisestus = scan.nextInt();
+            if (sisestus == 1) {
+                Arvutatav arvutaja = new BernoulliValem();
+                arvutaja.näide();
+                arvutaja.arvuta();
+            }
+            else if (sisestus == 2) {
+                Arvutatav arvutaja = new Multinoom();
+                arvutaja.näide();
+                arvutaja.arvuta();
+            }
+            else if (sisestus == 3) {
+                Arvutatav arvutaja = new Hüpergeomeetriline();
+                arvutaja.näide();
+                arvutaja.arvuta();
+            }
+            else if (sisestus == 4) {
+                Arvutatav arvutaja = new BernoulliJaotus();
+                arvutaja.näide();
+                arvutaja.arvuta();
+            }
+            else if (sisestus == 5) {
+                Arvutatav arvutaja = new BinoomJaotus();
+                arvutaja.näide();
+                arvutaja.arvuta();
+            }
+            else if (sisestus == 6) {
+                Arvutatav arvutaja = new GeomeetrilineJaotus();
+                arvutaja.näide();
+                arvutaja.arvuta();
+            }
+            else if (sisestus == 7){
+                Arvutatav arvutaja = new PoissoniJaotus();
+                arvutaja.näide();
+                arvutaja.arvuta();
+            }
+        }
     }
 }
