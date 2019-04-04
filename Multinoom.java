ed361eafce489229d191d37d699d6198e2ca5f1d
somermaret <somermaret@gmail.com>
2019-04-04 09:45:22 +0300

    tegelik multinoomjaotuse valem

--- Multinoom.java
+++ Multinoom.java
@@ -1,98 +1,116 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Multinoom implements Arvutatav {
 
-    private int suurN;
+    private int n;
     private int m;
-    private int väikeN;
 
-    public void setSuurN(int suurN) {
-        this.suurN = suurN;
+    public void setN(int n) {
+        this.n = n;
     }
 
     public void setM(int m) {
         this.m = m;
     }
 
-    public void setVäikeN(int väikeN) {
-        this.väikeN = väikeN;
-    }
-
     public Multinoom() {
-        this.suurN = 0;
+        this.n = 0;
         this.m = 0;
-        this.väikeN = 0;
-    }
-
-    static long kombinatsioonid(int k, int n) {
-        long lugeja = 1L;
-        long nimetaja = 1L;
-        if (k > n - k) {
-            for (int i = k + 1; i < n + 1; i++) {
-                lugeja *= i;
-            }
-            for (int i = 1; i < n - k + 1; i++) {
-                nimetaja *= i;
-            }
-        } else {
-            for (int i = n - k + 1; i < n + 1; i++) {
-                lugeja *= i;
-            }
-            for (int i = 1; i < k + 1; i++) {
-                nimetaja *= i;
-            }
-        }
-        return lugeja / nimetaja;
     }
 
     @Override
     public void näide() {
         setM((int) Math.round(2 + Math.random() * 3));
-        int[] klassid = new int[m];
+        double[] tõenäosused = new double[m];
         int[] k = new int[m];
         int kSumma = 0;
-        int nSumma = 0;
-        long lugeja = 1;
+        double pSumma = 0;
         for (int i = 0; i < m; i++) {
-            klassid[i] = (int) Math.round(1 + Math.random() * 4);
-            k[i] = (int) Math.round(Math.random() * klassid[i]);
+            k[i] = (int) Math.round(Math.random() * 7);
             kSumma += k[i];
-            nSumma += klassid[i];
-            lugeja *= kombinatsioonid(k[i], klassid[i]);
+            if (i != m - 1) {
+                tõenäosused[i] = Math.random() * (1 - pSumma);
+                pSumma += tõenäosused[i];
+            }
         }
-        setSuurN(nSumma);
-        setVäikeN(kSumma);
-        long nimetaja = kombinatsioonid(väikeN, suurN);
-        double tõenäosus = (double) lugeja / (double) nimetaja;
-        System.out.println("Näide:\nKui " + suurN + " objekti on jagatud " + m + " klassi järgmiselt:\n" + Arrays.toString(klassid) + "\n" +
-                "siis " + väikeN + " objekti valimisel on tõenäosus, et klassidest valitakse vastavalt\n" + Arrays.toString(k) + "\nobjekti," +
-                " P=" + (Math.round(tõenäosus * 1000) / 1000.0) + "\n");
+        tõenäosused[m - 1] = 1.0 - pSumma;
+        setN(kSumma);
+        BigDecimal faktoriaalLugeja = new BigDecimal("1");
+        for (int i = 2; i < n + 1; i++) {
+            faktoriaalLugeja = faktoriaalLugeja.multiply(BigDecimal.valueOf(i));
+        }
+        BigDecimal faktoriaalNimetaja = new BigDecimal("1");
+        for (int i = 0; i < m; i++) { //käib läbi kõik k-d
+            for (int j = 2; j < k[i] + 1; j++) { //korrutab nimetajaga k faktoriaali
+                faktoriaalNimetaja = faktoriaalNimetaja.multiply(BigDecimal.valueOf(j));
+            }
+        }
+        double tõenäosus = faktoriaalLugeja.divide(faktoriaalNimetaja, 3, RoundingMode.HALF_UP).doubleValue();
+        for (int i = 0; i < m; i++) {
+            tõenäosus *= Math.pow(tõenäosused[i], k[i]);
+        }
+        System.out.print("Näide:\nTõenäosus, et " + n + " sõltumatus katses sündmused C(1), C(2)");
+        if (m == 3){
+            System.out.print(", C(3)");
+        }
+        if (m > 3){
+            System.out.print(", ..., C(" + m + ")");
+        }
+        System.out.print(", mille tõenäosused on vastavalt [" + (Math.round(tõenäosused[0] * 1000.0) / 1000.0));
+        for (int i = 1; i < m; i++) {
+            System.out.print(", " + Math.round(tõenäosused[i] * 1000.0) / 1000.0);
+        }
+        System.out.println("]\ntoimuvad vastavalt " + Arrays.toString(k) + " korda, on: " + (Math.round(tõenäosus * 100000.0) / 100000.0) + "\n");
     }
 
     public void arvuta() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Sisesta m (klasside koguarv)");
+        System.out.println("Sisesta m (katsel toimuvate võimalike sündmuste koguarv)");
         setM(scan.nextInt());
-        int[] klassid = new int[m];
+        double[] tõenäosused = new double[m];
         int[] k = new int[m];
         int kSumma = 0;
-        int nSumma = 0;
-        long lugeja = 1;
+        double pSumma = 0;
         for (int i = 0; i < m; i++) {
-            System.out.println("Sisesta N(" + (i + 1) + ") ehk mitu objekti vastavasse klassi kuulub");
-            klassid[i] = scan.nextInt();
-            System.out.println("Sisesta k(" + (i + 1) + ") ehk mitu objekti sellest klassist valitakse");
+            System.out.println("Sisesta k(" + (i + 1) + ") ehk mitu korda vastav sündmus toimuma peab");
             k[i] = scan.nextInt();
             kSumma += k[i];
-            nSumma += klassid[i];
-            lugeja *= kombinatsioonid(k[i], klassid[i]);
+            if (i != m - 1) {
+                System.out.println("Sisesta p(" + (i + 1) + ") ehk vastava sündmuse toimumise tõenäosus");
+                tõenäosused[i] = scan.nextDouble();
+                pSumma += tõenäosused[i];
+            }
         }
-        setSuurN(nSumma);
-        setVäikeN(kSumma);
-        long nimetaja = kombinatsioonid(väikeN, suurN);
-        double tõenäosus = (double) lugeja / (double) nimetaja;
-        System.out.println("Tõenäosus, et " + suurN + "-st objektist, mis on jaotatud " + m + " klassi järgmiselt:\n" + Arrays.toString(klassid) + "\n" +
-                väikeN + " objekti valimisel saadakse klassidest vastavalt\n" + Arrays.toString(k) + "\nobjekti, on: " + (Math.round(tõenäosus * 1000) / 1000.0) + "\n");
+        tõenäosused[m - 1] = 1.0 - pSumma;
+        setN(kSumma);
+        BigDecimal faktoriaalLugeja = new BigDecimal("1");
+        for (int i = 2; i < n + 1; i++) {
+            faktoriaalLugeja = faktoriaalLugeja.multiply(BigDecimal.valueOf(i));
+        }
+        BigDecimal faktoriaalNimetaja = new BigDecimal("1");
+        for (int i = 0; i < m; i++) { //käib läbi kõik k-d
+            for (int j = 2; j < k[i] + 1; j++) { //korrutab nimetajaga k faktoriaali
+                faktoriaalNimetaja = faktoriaalNimetaja.multiply(BigDecimal.valueOf(j));
+            }
+        }
+        double tõenäosus = faktoriaalLugeja.divide(faktoriaalNimetaja, 3, RoundingMode.HALF_UP).doubleValue();
+        for (int i = 0; i < m; i++) {
+            tõenäosus *= Math.pow(tõenäosused[i], k[i]);
+        }
+        System.out.print("Näide:\nTõenäosus, et " + n + " sõltumatus katses sündmused C(1), C(2)");
+        if (m == 3){
+            System.out.print(", C(3)");
+        }
+        if (m > 3){
+            System.out.print(", ..., C(" + m + ")");
+        }
+        System.out.print(", mille tõenäosused on vastavalt [" + (Math.round(tõenäosused[0] * 1000.0) / 1000.0));
+        for (int i = 1; i < m; i++) {
+            System.out.print(", " + Math.round(tõenäosused[i] * 1000.0) / 1000.0);
+        }
+        System.out.println("]\ntoimuvad vastavalt " + Arrays.toString(k) + " korda, on: " + (Math.round(tõenäosus * 100000.0) / 100000.0));
     }
 }
