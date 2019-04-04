be0fdb58810e05b89c98f90e207d6fac34498673
somermaret <somermaret@gmail.com>
2019-04-04 09:51:48 +0300

    veel üks reavahetus

--- BernoulliJaotus.java
+++ BernoulliJaotus.java
@@ -27,7 +27,7 @@ public class BernoulliJaotus implements Arvutatav {
         System.out.println("Näide:\nKui juhuslik suurus X on Bernoulli jaotusega parameetriga p=" +
                 Math.round(p * 100.0) / 100.0 + ", siis on keskväärtus " +
                 Math.round(keskväärtus * 100.0) / 100.0 + ", dispersioon " + Math.round(dispersioon * 100.0) / 100.0 +
-                " ja standardhälve " + Math.round(standardhälve * 100.0) / 100.0 + ".");
+                " ja standardhälve " + Math.round(standardhälve * 100.0) / 100.0 + ".\n");
     }
 
     //Küsime kasutajalt vajalikud andmed ja väljastame soovitud tulemuse:
@@ -43,6 +43,6 @@ public class BernoulliJaotus implements Arvutatav {
 
         System.out.println("Bernoulli jaotuse kohaselt on keskväärtus " + Math.round(keskväärtus * 100.0) / 100.0
                 + ", dispersioon " + Math.round(dispersioon * 100.0) / 100.0 + " ja standardhälve "
-                + Math.round(standardhälve * 100.0) / 100.0 + ".");
+                + Math.round(standardhälve * 100.0) / 100.0 + ".\n");
     }
 }
